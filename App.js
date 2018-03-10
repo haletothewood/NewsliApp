f28b1ca4b948b5718e138d968931f23f6a1edc57
haletothewood <davidnhalewood@hotmail.com>
2018-03-10 16:44:11 +0000

    Add onRefresh and extract styles

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { StyleSheet, Text, View, Footer } from 'react-native'
+import { RefreshControl, ScrollView, StyleSheet, Text, View, Footer } from 'react-native'
 import { Font } from 'expo'
 import { Button } from 'react-native-elements'
+import Styles from './AppStyles.js'
 
 export default class App extends React.Component {
   state = {
     fontLoaded: false,
     buttonPressed: false,
-    showButton: true
+    showButton: true,
+    refreshing: false
   }
 
   async componentWillMount() {
@@ -41,64 +43,44 @@ export default class App extends React.Component {
     })
   }
 
+  onRefresh = () => {
+    this.setState({refreshing: true});
+    this.fetchArticlesAsync().then(() => {
+      this.setState({refreshing: false});
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-      {
-        this.state.fontLoaded &&
-        <View style={this.state.buttonPressed ? styles.newsContainer : styles.container}>
-          <Text style={styles.header}>Newsli</Text>
-          {this.state.showButton && 
-          <Button buttonStyle={styles.button} onPress={this.onPress} clear text='Feed Me' />}
-          <View>
-          {this.state.buttonPressed &&
-            <View>
-              {this.state.articleList ?
-              <View style={styles.newsContainer}>
-              {this.state.articleList.map((article) => {
-                return <Text style={styles.text}>{article.webTitle}</Text>
-              })}
-              </View> : <Text style={styles.loading}>Loading</Text>}
-            </View>}
+      <View style={Styles.container}>
+        {
+          this.state.fontLoaded &&
+          <View style={this.state.buttonPressed ? Styles.newsContainer:Styles.container}>
+            <Text style={Styles.header}>Newsli</Text>
+            {this.state.showButton && 
+            <Button buttonStyle={Styles.button} onPress={this.onPress} clear text='Feed Me' />}
+            <ScrollView
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={this.onRefresh}
+              />
+            }>
+              {this.state.buttonPressed &&
+              <View>
+                {this.state.articleList ?
+                <View style={Styles.articleContainer}>
+                {this.state.articleList.map((article, i) => {
+                  return <View style={Styles.articleContainer} key={i}>
+                    <Text style={Styles.text}>{article.webTitle}</Text>
+                    </View>
+                })}
+                </View> : <Text style={Styles.loading}>Loading</Text>}
+              </View>}
+            </ScrollView>
           </View>
-        </View>
-      }
+        }
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  newsContainer: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    marginTop: 15
-  },
-  header: {
-    fontFamily: 'Ubuntu', 
-    fontSize: 90,
-    color: '#fff'
-  },
-  text: {
-    fontFamily: 'Ubuntu-Light',
-    fontSize: 16,
-    color: '#fff'
-  },
-  loading: {
-    fontFamily: 'Ubuntu-Light',
-    fontSize: 24,
-    color: '#FF69B4'
-  },
-  button: {
-    borderColor: '#fff', 
-    borderWidth: 2
-  }
-})
---
+++ AppStyles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native'
+
+export default Styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    newsContainer: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      marginTop: 15
+    },
+    articleContainer: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      marginTop: 15
+    },
+    scroll:{
+  
+    },
+    header: {
+      fontFamily: 'Ubuntu', 
+      fontSize: 90,
+      color: '#fff'
+    },
+    text: {
+      fontFamily: 'Ubuntu-Light',
+      fontSize: 20,
+      color: '#fff',
+    },
+    loading: {
+      fontFamily: 'Ubuntu-Light',
+      fontSize: 24,
+      color: '#FF69B4'
+    },
+    button: {
+      borderColor: '#fff', 
+      borderWidth: 2
+    }
+  })
