7aea69d5b818eedb34a9a8b292bfed8266565cab
haletothewood <davidnhalewood@hotmail.com>
2018-03-11 13:48:39 +0000

    Convert to stateless functions

--- Article.js
+++ Article.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Styles from './AppStyles.js'
 
-export default class Article extends React.Component {
-
-    render(){
-        return <View style={Styles.articleContainer} key={this.props.id}>
-            <Text style={Styles.text}>{this.props.article.webTitle}</Text>
-        </View>
-    }
+const Article = (props) => {
+    return <View style={Styles.articleContainer} key={props.id}>
+        <Text style={Styles.text}>{props.article.webTitle}</Text>
+    </View>
 }
+
+export default Article
--- ArticleList.js
+++ ArticleList.js
@@ -3,12 +3,12 @@ import { RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native
 import Styles from './AppStyles.js'
 import Article from './Article.js'
 
-export default class ArticleList extends React.Component {
-
-    render(){
-        return this.props.articleList ?
-        this.props.articleList.map((article, i) => {
+const ArticleList = (props) => {
+    return props.articleList ?
+        props.articleList.map((article, i) => {
             return <Article article={article} key={i} id={i} />
-        }): <Text style={Styles.loading}>Loading</Text>
-    }
+        }) : 
+        <Text style={Styles.loading}>Loading</Text>
 }
+
+export default ArticleList
